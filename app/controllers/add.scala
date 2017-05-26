7024a4f7ba5050931a912a36b600205c31371eab
mox <c38p8d@gmail.com>
2017-05-26 20:40:39 +0900

    ファイルの追加

---
+++ app/controllers/add.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject.Inject
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc._
+import play.api.i18n.Messages.Implicits._
+import play.api.Play._
+import formModel._
+
+class AddController () extends Controller {
+}
