9947bae88da58733636aad514d0c2780fb1f6586
mox <c38p8d@gmail.com>
2017-05-31 15:47:13 +0900

    DB用Modelを追加

---
+++ app/databaseModels/WatersModel.scala
@@ -0,0 +1,37 @@
+package databaseModels
+
+import javax.inject._
+import play.api.db._
+import anorm._
+import anorm.SqlParser._
+
+@Singleton
+class WatersModel(db: Database) {
+	val waterIdParser = str("id")
+	val waterIdMapper = waterIdParser.map { 
+		case id => Map("id" -> id)
+	}
+
+	def isExistId(id: String): Boolean = {
+		db.withConnection { implicit connect =>
+			val idData = SQL("SELECT `id` FROM `waters` WHERE `id` = {Id};")
+			.on("Id" -> id).as(waterIdMapper.*)
+			
+			return idData.nonEmpty
+		}
+	}
+
+    def addId(id: String) = {
+        db.withTransaction{implicit connect =>
+            SQL("INSERT INTO `waters`(`id`, `status`) values({Id}, 0);")
+				.on("Id" -> id).executeInsert()
+        }
+    }
+
+	def updateTime(id: String, unixTime: Int) = {
+        db.withTransaction{implicit connect =>
+            SQL("UPDATE `waters` SET `time` = {Time} WHERE `id` = {Id};")
+                .on("Time" -> unixTime, "Id" -> id).executeInsert()
+        }
+    }
+}
