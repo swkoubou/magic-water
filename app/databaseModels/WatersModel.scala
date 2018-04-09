0802de966f47d8646d3788f83523e6354f793e49
mox <mox@g.mox.si>
2018-04-09 11:28:42 +0900

    ソースコードの整形

--- app/databaseModels/WatersModel.scala
+++ app/databaseModels/WatersModel.scala
@@ -11,10 +11,10 @@ case class WaterDbData(id:String, name:String, status:Option[Int], time:Option[I
 @Singleton
 class WatersModel(db: Database) {
     //Id用パーサー
-	val waterIdParser = str("id")
-	val waterIdMapper = waterIdParser.map { 
-		case id => Map("id" -> id)
-	}
+    val waterIdParser = str("id")
+    val waterIdMapper = waterIdParser.map {
+        case id => Map("id" -> id)
+    }
 
     //データ取得用パーサー
     val waterDataParser = str("id") ~ str("name") ~ get[Option[Int]]("status") ~ get[Option[Int]]("time") ~ get[Option[Int]]("category")
@@ -22,14 +22,14 @@ class WatersModel(db: Database) {
         case id~name~status~time~category => WaterDbData(id, name, status, time, category)
     }
 
-	def isExistId(id: String): Boolean = {
-		db.withConnection { implicit connect =>
-			val idData = SQL("SELECT `id` FROM `waters` WHERE `id` = {Id};")
-			    .on("Id" -> id).as(waterIdMapper.*)
-			
-			return idData.nonEmpty
-		}
-	}
+    def isExistId(id: String): Boolean = {
+        db.withConnection { implicit connect =>
+            val idData = SQL("SELECT `id` FROM `waters` WHERE `id` = {Id};")
+              .on("Id" -> id).as(waterIdMapper.*)
+
+            return idData.nonEmpty
+        }
+    }
 
     def addId(id: String, name: String, category: Option[Int]) = {
         var categoryId = -1
@@ -40,49 +40,49 @@ class WatersModel(db: Database) {
 
         db.withTransaction{implicit connect =>
             SQL("INSERT INTO `waters`(`id`, `name`, `status`, `category`) values({Id}, {Name}, 0, {Category});")
-				.on("Id" -> id, "Name" -> name, "Category" -> categoryId).executeInsert()
+              .on("Id" -> id, "Name" -> name, "Category" -> categoryId).executeInsert()
         }
     }
 
-	def updateTime(id: String, unixTime: Int) = {
+    def updateTime(id: String, unixTime: Int) = {
         db.withTransaction{implicit connect =>
             SQL("UPDATE `waters` SET `time` = {Time} WHERE `id` = {Id};")
-                .on("Time" -> unixTime, "Id" -> id).executeUpdate()
+              .on("Time" -> unixTime, "Id" -> id).executeUpdate()
         }
     }
 
     def updateStatus(id: String, statusCode: Int) = {
         db.withTransaction{implicit connect =>
             SQL("UPDATE `waters` SET `status` = {Status} WHERE `id` = {Id};")
-                .on("Status" -> statusCode, "Id" -> id).executeUpdate()
+              .on("Status" -> statusCode, "Id" -> id).executeUpdate()
         }
     }
 
     def updateName(id: String, name: String) = {
         db.withTransaction{implicit connect =>
             SQL("UPDATE `waters` SET `name` = {Name} WHERE `id` = {Id};")
-                .on("Name" -> name, "Id" -> id).executeUpdate()
+              .on("Name" -> name, "Id" -> id).executeUpdate()
         }
     }
 
     def updateCategory(id: String, categoryCode: Int) = {
         db.withTransaction{implicit connect =>
             SQL("UPDATE `waters` SET `category` = {Category} WHERE `id` = {Id};")
-                .on("Category" -> categoryCode, "Id" -> id).executeUpdate()
+              .on("Category" -> categoryCode, "Id" -> id).executeUpdate()
         }
     }
 
     def delete(id: String) = {
         db.withTransaction{implicit connect =>
-          SQL("DELETE FROM `waters` WHERE `id` = {id};")
-            .on("id" -> id).executeUpdate()
+            SQL("DELETE FROM `waters` WHERE `id` = {id};")
+              .on("id" -> id).executeUpdate()
         }
     }
 
     def getAll(): List[WaterData] = {
         db.withConnection{implicit connect =>
             val data = SQL("SELECT * FROM `waters` WHERE 1;")
-                .as(waterDataMapper.*)
+              .as(waterDataMapper.*)
 
             var returnData:List[WaterData] = List[WaterData]()
             for(value <- data){
@@ -102,7 +102,7 @@ class WatersModel(db: Database) {
     def getFromId(id: String): WaterData = {
         db.withConnection{implicit connect =>
             val data = SQL("SELECT * FROM `waters` WHERE `id` = {Id};")
-                .on("Id" -> id).as(waterDataMapper.*)
+              .on("Id" -> id).as(waterDataMapper.*)
 
             var returnData:List[WaterData] = List[WaterData]()
             for(value <- data){
